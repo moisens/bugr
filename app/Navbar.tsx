3a9fccbd5ebd5f548055092f01e61e0a8685731f
moise <mose.ns@hotmail.com>
2023-10-07 17:20:20 +0200

    Styling the active link

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,22 +1,32 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 import { links } from "@/utils/utils";
 
 const Navbar = () => {
+  const currentPath = usePathname();
+
   return (
-    <nav className="flex space-x-6 border-b  mb-5 px-5 h-14 items-center">
-      <Link href="/">bugr</Link>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/" className="font-semibold text-2xl text-orange-800">
+        bugr
+      </Link>
       <ul className="flex space-x-6">
         {links.map((link) => {
-          const { id, label, href } = link;
+          const { id, label, hrefLink } = link;
           return (
             <li
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classnames({
+                "text-zinc-900": hrefLink === currentPath,
+                "text-zinc-500": hrefLink !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
               key={id}
             >
-              <Link href={href}>{label}</Link>
+              <Link href={hrefLink}>{label}</Link>
             </li>
           );
         })}
