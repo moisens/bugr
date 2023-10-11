bc6a52879375faedb1b532cfa3ae028c99fc47ab
moise <mose.ns@hotmail.com>
2023-10-11 16:03:21 +0200

    building patch api

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createIssuesSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+
+export const PATCH = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const body = await request.json();
+  const validation = createIssuesSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json(
+      { error: "It appear that issue doesn't exist!" },
+      { status: 404 }
+    );
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+};
