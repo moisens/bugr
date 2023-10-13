952a2caca29ab586cb2df09d81657de6d7184324
moise <mose.ns@hotmail.com>
2023-10-13 17:39:51 +0200

    Adding Login and Logout links

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/prisma/client";
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
