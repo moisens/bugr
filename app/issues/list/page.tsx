07a9a0fb101cd0a3e991290e3873db42fc67a186
moise <mose.ns@hotmail.com>
2023-10-17 23:55:40 +0200

    Sorting issues on the UI

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -23,8 +23,15 @@ const IsssuesPage = async ({ searchParams }: SearchParamsType) => {
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
   //await delay(2000)
 
