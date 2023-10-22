068231b76a8de6706488aa1dfb8ba676c5748f62
moise <mose.ns@hotmail.com>
2023-10-22 23:54:21 +0200

    Issue Summary Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import IssuesSummary from "./IssuesSummary";
 import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
 
+const Home = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
 
-export default function Home() {
   return (
-    <LatestIssues />
+    <>
+      <LatestIssues />
+      <IssuesSummary open={open} inProgress={inProgress} closed={closed} />
+    </>
   );
-}
+};
+
+export default Home;
